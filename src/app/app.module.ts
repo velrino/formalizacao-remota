12dbb3495ca02b82ff98d328fee2a87052bab879
velrino <denismagalhaesvelrino@gmail.com>
2020-04-16 20:52:18 -0300

    :art: imported AppModuleComponents and AppRoutingModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppModuleComponents, AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...AppModuleComponents
   ],
   imports: [
     BrowserModule,
