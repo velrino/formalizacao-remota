93b0fca2895da2aeab3a2269523fa56e208dc043
velrino <denismagalhaesvelrino@gmail.com>
2020-04-17 00:46:45 -0300

    :art: improved header with title

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  @Input('title') title: string;
+  
   constructor() { }
 
   ngOnInit(): void {
