0d9597a475976cce234b5eb08fcb08fa8a99755b
velrino <denismagalhaesvelrino@gmail.com>
2020-04-16 22:34:37 -0300

    :art: improved effectuation

--- src/app/pages/effectuation/effectuation.component.ts
+++ src/app/pages/effectuation/effectuation.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./effectuation.component.scss']
 })
 export class EffectuationComponent implements OnInit {
+  data = {
+    nome: 'Lorem Ipsum da Silva',
+    limite: 2000,
+    anuidade: 1,
+    contratacao: 'aceita'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  enumAnuidade() {
+    const tiposAnuidade = [
+      'isento',
+      'não isento',
+    ]
+    return tiposAnuidade[this.data.anuidade];
+  }
+
+  enumContratacao() {
+    const tiposContratacao = {
+      'aceita': 'Aceita',
+      'recusar': 'Recusada',
+    }
+    return tiposContratacao[this.data.contratacao].toLowerCase();
+  }
 }
