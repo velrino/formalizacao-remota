1909d015253f1dcc1defb2f847bbb3fa315c1a09
velrino <denismagalhaesvelrino@gmail.com>
2020-04-20 19:58:34 -0300

    :zap: create toggle for handle show/hide class accessibility

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,10 +7,37 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input('title') title: string;
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  component = {
+    accessibility: {
+      elements: null,
+      className: "accessibility",
+      classNameShow: "accessibility-show",
+    }
   }
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  toggleAccessibility() {
+    const { accessibility: { className } } = this.component;
+    let elements = document.getElementsByClassName(className);
+    for (let i in elements) {
+      let element = elements[i];
+      this.toggleHasElementClassList(element);
+    }
+  }
+
+  toggleHasElementClassList(element) {
+    if (typeof element.classList != "undefined") {
+      this.toggleClass(element);
+    }
+  }
+
+  toggleClass(element) {
+    const { accessibility: { classNameShow } } = this.component;
+    const hasClass = element.classList.contains(classNameShow);
+    (hasClass) ? element.classList.remove(classNameShow) : element.classList.add(classNameShow);
+  }
 }
